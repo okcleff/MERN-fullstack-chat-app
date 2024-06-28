1301f3694a02b2606c27034d74c8c68f769c1f34
okcleff <okcleff@gmail.com>
2024-06-29 00:36:42 +0900

    add logout api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -42,13 +42,16 @@ export const signUpUser = async (req, res) => {
       await newUser.save();
 
       res.status(201).json({
-        _id: newUser._id,
-        fullName: newUser.fullName,
-        username: newUser.username,
-        profilePic: newUser.profilePic,
+        result: true,
+        data: {
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          username: newUser.username,
+          profilePic: newUser.profilePic,
+        },
       });
     } else {
-      res.status(400).json({ error: 'Invalid user data' });
+      res.status(400).json({ result: false, message: 'Invalid user data' });
     }
   } catch (error) {
     console.log('Error in signup controller', error.message);
@@ -62,20 +65,25 @@ export const loginUser = async (req, res) => {
     const user = await User.findOne({ username });
     const isPasswordCorrect = await bcrypt.compare(
       password,
-      user?.password || ''
+      user?.password || '' // user가 존재하지 않을 수 있으므로 옵셔널 체이닝 사용
     );
 
     if (!user || !isPasswordCorrect) {
-      return res.status(400).json({ error: 'Invalid username or password' });
+      return res
+        .status(400)
+        .json({ result: false, message: 'Invalid username or password' });
     }
 
     generateTokenAndSetCookie(user._id, res);
 
     res.status(200).json({
-      _id: user._id,
-      fullName: user.fullName,
-      username: user.username,
-      profilePic: user.profilePic,
+      result: true,
+      data: {
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        profilePic: user.profilePic,
+      },
     });
   } catch (error) {
     console.log('Error in login controller', error.message);
@@ -84,5 +92,11 @@ export const loginUser = async (req, res) => {
 };
 
 export const logoutUser = (req, res) => {
-  res.send('Logout User');
+  try {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.status(200).json({ result: true, message: 'Logged out successfully' });
+  } catch (error) {
+    console.log('Error in logout controller', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
